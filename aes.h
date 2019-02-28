0fae26471d18379a002048b3f1fee8948d9dcfe6
dawson-brown <dawsonstbrown@gmail.com>
2019-02-27 20:17:01 -0500

    simplified AES--removed mix columns function. Simplified multiplcation function.

--- aes.h
+++ aes.h
@@ -1,13 +1,14 @@
 #include <stdlib.h>
-#include <string.h>
 
 #define NB 4
 #define NK 4
-#define NK_BLOCK 16
+#define BLOCK_SIZE 16
+#define KEY_SIZE 16
 #define NR 10
 
-//Key expansion
-#define EXPAND(k) for (int j=0; j<NK_BLOCK; j++) k[j] = j<4 ? k[j]^sbox[k[(j+13)-(j/3)*4]]^(j==0 ? rcon[i] : 0) : k[j]^k[j-NK];
+#define ROUNDS i=0; i<NR; i++
+#define KEY j=0; j<KEY_SIZE; j++
+#define BLOCK j=0; j<BLOCK_SIZE; j++
 
 //shift operations
 #define SHIFT_1(v,w,x,y,z) ( (v)=(w), (w)=(x), (x)=(y), (y)=(z), (z)=(v) )
@@ -15,14 +16,10 @@
 #define SHIFT_3(v,w,x,y,z) ( (v)=(w), (w)=(z), (z)=(y), (y)=(x), (x)=(v) )
 #define SHIFT_ROWS(x,y) ( SHIFT_1(x,y[1],y[5],y[9],y[13]), SHIFT_2(x,y[2],y[6],y[10],y[14]), SHIFT_3(x,y[3],y[7],y[11],y[15]) )
 
-//SBOX
-#define SBOX(x) for (int j=0; j<NK_BLOCK; j++) x[j] = sbox[x[j]];
-
-//add round key
-#define ADD_ROUND_KEY(x,y) for (int j=0; j<(NK_BLOCK); j++) x[j] ^= y[j];
-
 typedef unsigned char aes_t;
 
-void mix_columns(unsigned char *state);
+unsigned char sub(unsigned char byte);
+
+unsigned char mul(unsigned char a, unsigned char b);
 
 void aes(aes_t *state, unsigned char *key);
--- main.c
+++ main.c
@@ -1,16 +1,17 @@
 #include "aes.h"
+#include <string.h>
 #include <stdio.h>
 
 int main()
 {
     unsigned char plaintext[16] = {'\x32','\x43','\xf6','\xa8','\x88','\x5a','\x30','\x8d','\x31','\x31','\x98','\xa2','\xe0','\x37','\x07','\x34'};
     unsigned char key[16] = {'\x2b','\x7e','\x15','\x16','\x28','\xae','\xd2','\xa6','\xab','\xf7','\x15','\x88','\x09','\xcf','\x4f','\x3c'};
+
     aes(plaintext, key);
     
-    for(int i=0; i<NK_BLOCK; i++){
+    for(int i=0; i<BLOCK_SIZE; i++){
         printf("%02x", plaintext[i]);
     }
     printf("\n");
-
     return 0;
 }

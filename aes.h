0a6fbabd20d9c776eeeba4614ef5786b6cf09487
dawson-brown <dawsonstbrown@gmail.com>
2019-04-03 15:52:30 -0400

    finish decryption

--- aes.h
+++ aes.h
@@ -1,5 +1,3 @@
-#include <stdlib.h>
-
 #define NB 4
 #define NK 4
 #define BLOCK_SIZE 16
@@ -8,11 +6,14 @@
 
 #define ROUNDS i=0; i<NR; i++
 #define KEY j=0; j<KEY_SIZE; j++
+#define KEY_I j=KEY_SIZE-1; j>=0; j--
 #define BLOCK j=0; j<BLOCK_SIZE; j++
 
 typedef unsigned char aes_t;
 
 unsigned char sub(unsigned char byte);
+
+unsigned char sub_i(unsigned char byte);
 
 unsigned char mul(unsigned char a, unsigned char b);
 
